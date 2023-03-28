8a72eff8918b22c9c288904bf2ae90b253094448
Jacob Sniff <jacobsniff18@gmail.com>
2023-03-28 17:33:32 -0400

    Fixed bugs in unordered list and license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,26 +1,25 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// Function that returns a license badge
 function renderLicenseBadge(license) {
   if (!license) {
     return '';
   }
   let badgeUrl = '';
 
-  if (license.toLowerCase() === 'MIT') {
+  if (license.toLowerCase() === 'mit') {
     badgeUrl = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
-  } else if (license.toLowerCase() === 'APACHE') {
+  } else if (license.toLowerCase() === 'apache') {
     badgeUrl = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
-  } else if (license.toLowerCase() === 'GPL') {
+  } else if (license.toLowerCase() === 'gdl') {
     badgeUrl = '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
-  } else if (license.toLowerCase() === 'BSD') {
+  } else if (license.toLowerCase() === 'bsd') {
     badgeUrl = '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
   }
 
   return badgeUrl;
 };
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// Function that returns the license link
+
 function renderLicenseLink(license) {
   if (!license) {
     return '';
@@ -42,8 +41,7 @@ function renderLicenseLink(license) {
 };
 
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+// Function that returns the license section of README
 
 function renderLicenseSection(license) {
   if (!license) {
@@ -82,46 +80,45 @@ This project is licensed under the terms of the [BSD 3-Clause License](${renderL
 };
 
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.projectTitle}
 
   ${renderLicenseBadge(data.license)}
 
-  ## Description
+## Description
 
   ${data.projectDescription}
 
-  ## Table of Contents
+## Table of Contents
 
-  -[Installation](#installation)
-  -[Usage](#usage)
-  -[Contributing](#contributing)
-  -[Tests](#tests)
-  -[License](#license)
-  -[Questions](#questions)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
 
-  ## Installation
+## Installation
 
   ${data.installationInstructions}
 
-  ## Usage
+## Usage
 
   ${data.usageInformation}
 
-  ## Contributing
+## Contributing
 
   ${data.contributionGuidelines}
 
-  ## Tests
+## Tests
 
   ${data.testInstructions}
   ${renderLicenseSection(data.license)}
 
-  ## Questions
+## Questions
 
   If you have any questions, concerns, or comments please reachout to me on [Github](https://github.com/${data.githubUsername} or at ${data.email}).
-
 `;
 }
 
